915e396213a1fd6e100d43e1d5cd4baef0c91a72
esmameral <esma.meral@gmail.com>
2023-10-22 15:37:51 +0300

    Week4: initial commit for Mock testing

--- firstproject/src/main/java/com/ozyegin/firstproject/beans/MessageBeanTrImpl.java
+++ firstproject/src/main/java/com/ozyegin/firstproject/beans/MessageBeanTrImpl.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component("tr")
-@Profile("turkish")
+@Profile("!english")
 public class MessageBeanTrImpl implements MessageBean {
 
 	@Override
