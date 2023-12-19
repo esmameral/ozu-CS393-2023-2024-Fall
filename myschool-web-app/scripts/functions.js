4f0c570c912621322fb33f4aa130309d34562ae4
esmameral <esma.meral@gmail.com>
2023-12-20 01:15:02 +0300

    initial commit

---
+++ myschool-web-app/scripts/functions.js
@@ -0,0 +1,8 @@
+function sayHello2() {
+    console.log("this is a message");
+    element = document.getElementById("message1");
+    element.innerHTML = "New Header....";
+
+    document.getElementById("message2").style.color = "blue";
+
+}
