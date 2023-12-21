06ba1b2cfc5358246b75cec0b0b8b65b745e1141
esmameral <esma.meral@gmail.com>
2023-12-21 23:02:48 +0300

    minimal changes

--- myschool/src/main/java/com/demo/myschool/model/Course.java
+++ myschool/src/main/java/com/demo/myschool/model/Course.java
@@ -14,7 +14,6 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.Transient;
 
 
 @Entity
